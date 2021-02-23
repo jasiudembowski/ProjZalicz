c673c34032a45af9f8456416015bb1ce71b265f7
jasiudembowski <danuta_dembowska@wp.pl>
2021-02-23 17:31:07 +0100

    zad 3&4

--- src/Human.java
+++ src/Human.java
@@ -4,6 +4,22 @@ public class Human {
     private String name;
     private String age;
     private Car car;
+    private double salary;
+
+
+    public double getSalary(){
+        return this.salary;
+    }
+
+    public void setSalary(double salary){
+        if(salary < 0 ) return;
+        System.out.println("Nowe dane zostały wyslane do systemu ksiegowego");
+        System.out.println("Koniecznosc odebrania aneksu do umowy od Pani Hani z kadr");
+        System.out.println("ZUS i US juz wiedza o zmianie dochodu i nie ma sensu ukrywac dochodu");
+        this.salary = salary;
+        
+    }
+
 
     public void setCar(Car car){
         this.car = car;
